56ead6d08cc7b41b2d92f3b2f1db0a1300e03c55
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-03-18 15:29:51 +0300

    [FIR] Move all checkers and theirs infrastructure to separate module

--- build.gradle.kts
+++ build.gradle.kts
@@ -261,6 +261,7 @@ extra["compilerModules"] = arrayOf(
     ":compiler:fir:fir2ir",
     ":compiler:fir:java",
     ":compiler:fir:jvm",
+    ":compiler:fir:checkers",
     ":compiler:fir:analysis-tests"
 )
 
